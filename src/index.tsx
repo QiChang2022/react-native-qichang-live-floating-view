dc134ebe814978b2e3ffe0af6de389788b716214
Damoness <qjwdcool@qq.com>
2021-03-03 17:01:12 +0800

    fix: add onPress

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import { requireNativeComponent, ViewStyle } from 'react-native';
 
 type QichangLiveFloatingViewProps = {
   style: ViewStyle;
+  onPress?: () => void;
 };
 
 export const QichangLiveFloatingView = requireNativeComponent<QichangLiveFloatingViewProps>(
